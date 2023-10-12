6a41716d06639f317d7746f1bf9a8e03bb6212f4
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2023-10-11 21:36:19 -0300

    Refatorando o projeto base.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,15 +1,10 @@
-import Link from "next/link";
 
 export default function Home(){
 
   return(
     <div>
        <h1>Home</h1>
-        <ul>
-          <li><Link href="/post">POST</Link></li>
-          <li><Link href="/post/post1">POST-1</Link></li>
-          <li><Link href="/post/post2">POST-2</Link></li>
-        </ul>     
+   
     </div>
   )
 }
